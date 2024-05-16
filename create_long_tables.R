c8afafd3561a7b350e1e23c09d46f293636ff661
Elise Hellwig <echellwig@ucdavis.edu>
2024-05-15 19:06:58 -0700

    creating long course related tables

---
+++ create_long_tables.R
@@ -0,0 +1,142 @@
+library('readxl')
+library('data.table')
+library('stringr')
+library('sqldf')
+
+source('functions.R')
+#str_multi_replace
+#strsplit_vector
+#remove_duplicates
+
+desc = fread('data/course_descriptions.csv')
+
+# Create GE table ---------------------------------------------------------
+
+ge_replace = data.table(string=c('\\),', ' or '), replacement=c('\\);', '; '))
+
+GEs = desc$Gen_Ed[!is.na(desc$Gen_Ed)] |>
+  str_multi_replace(ge_replace) |>
+  strsplit_vector(';') |>
+  str_replace_all('\\.', '') |>
+  trimws() |>
+  unique() |>
+  str_replace_all('\\)', '') |>
+  str_split_fixed(' \\(', 2) |>
+  data.table()
+
+setnames(GEs, names(GEs), c('ge_name', 'ge_code'))
+
+GEs = GEs[ge_code !='']
+
+GEs = GEs[order(GEs$ge_name)]
+GEs[,ge_id:= 1:nrow(GEs)]
+
+fwrite(GEs, 'data/tables/ge.csv')
+
+
+# Create GE course table --------------------------------------------------
+
+
+desc_ge = desc[ , .(cn, Gen_Ed)]
+
+ge_query <- paste("SELECT cn, ge_id FROM desc_ge INNER JOIN GEs",
+                  "ON Gen_Ed LIKE CONCAT('%', ge_name, '%');")
+
+ge_course = sqldf(ge_query) |> data.table()
+
+fwrite(ge_course, 'data/tables/ge_course.csv')
+
+# ge_course = fuzzy_join(desc_ge, GEs, by= c('Gen_Ed' = 'ge_name'), 
+#                        match_fun = str_detect, mode='inner') 
+# 
+# ge_course = ge_course[, c('cn', 'ge_id')]
+
+
+# Correct Misspellings ----------------------------------------------------
+
+act_misspell = data.table(string=c('Discusson', 'Intership'), 
+                          replacement=c('Discussion', 'Internship'))
+
+desc[, Activities:= str_multi_replace(Activities, act_misspell)]
+
+# Create Learning Activities Table ----------------------------------------
+
+
+act_remove = c('hour\\(s\\)', 'hour\\(s\\(', 'hour \\(s\\)', 'hours\\(s\\)',
+               '[0-9,\\.\\-]')
+
+act_replace = data.table(string=c(act_remove, '\\s\\s+'), 
+                         replacement = c(rep('', length(act_remove)), ' '))
+
+acts = desc$Activities[!is.na(desc$Activities)] |>
+  tolower() |>
+  strsplit_vector('[,;\\] ') |>
+  strsplit_vector('/') |>
+  str_multi_replace(act_replace) |>
+  str_replace_all('[\\s]', ' ') |>
+  trimws() |>
+  unique()
+
+good_acts =  acts!='' & !grepl('\\)', acts) & !grepl('consent', acts) 
+
+acts = acts[good_acts]
+
+acts = remove_duplicates(acts) |> sort()
+
+learning_activity = data.table(activity_id = 1:length(acts),
+                               activity = acts)
+
+fwrite(learning_activity, 'data/tables/learning_activity.csv')
+
+
+# Create course activity table --------------------------------------------
+
+
+act_query <- paste("SELECT cn, activity_id ",
+                   "FROM desc INNER JOIN learning_activity",
+                   "ON LOWER(Activities) LIKE CONCAT('%', activity, '%');")
+
+act_course = sqldf(act_query) |> data.table()
+
+fwrite(act_course, 'data/tables/course_activity.csv')
+
+# Create crosslisting -----------------------------------------------------
+
+cl_replace = data.table(string=c(';', '\\.$'), replacement=c(', ', ''))
+
+cl_wide = desc[!is.na(Cross_Listing), .(cn, Cross_Listing)]
+
+cl_wide[, Cross_Listing:=toupper(Cross_Listing)]
+
+cl_wide[, Cross_Listing:=str_multi_replace(Cross_Listing, cl_replace)]
+
+cl = lapply(1:nrow(cl_wide), function(i) {
+  cn_vec = str_split_1(cl_wide$Cross_Listing[i], ', ')
+  
+  data.table(cn=cl_wide$cn[i], Cross_Listing = cn_vec)
+}) |> rbindlist()
+
+cl[, Cross_Listing:=gsub(' ', '', Cross_Listing)]
+
+
+# Prerequisities Table ----------------------------------------------------
+
+course[, c_n:= paste(Subject, Number)]
+cns = course[, .(cn, c_n)]
+setnames(cns, names(cns), paste0('pr_', names(cns)))
+
+desc[, ":="(instructor_consent = grepl('consent', tolower(Prerequisites)),
+            upper_division = grepl('upper division', tolower(Prerequisites)),
+            graduate = grepl('graduate', tolower(Prerequisites)))]
+
+desc_prereq = desc[!is.na(Prerequisites), .(cn, Prerequisites)]
+
+prereq_query = paste("SELECT cn, pr_cn FROM desc_prereq INNER JOIN cns",
+                     "ON Prerequisites LIKE CONCAT('%', pr_c_n, '%');")
+
+course_prereq = sqldf(prereq_query) |> data.table()
+
+
+
+
+
