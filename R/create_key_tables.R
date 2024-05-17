2e26410fad39e4cdcc701edc410b0b83136de8e1
Elise Hellwig <echellwig@ucdavis.edu>
2024-05-17 12:02:59 -0700

    updating script to reflect changes in the cleaning process.

--- R/create_key_tables.R
+++ R/create_key_tables.R
@@ -1,6 +1,4 @@
 # ges, learning activities, grade_type, restriction type
-
-library('readxl')
 library('data.table')
 library('stringr')
 library('DBI')
@@ -15,17 +13,23 @@ source('R/db_functions.R')
 
 con <- ucd_course_connect('elisehellwig')
 
+
 # Load Data ---------------------------------------------------------------
 
 grade_type = fread('data/tables/grade_type.csv')
+restr_type = fread('data/tables/restriction_type.csv', sep=',')
+course = fread('data/courses_clean.csv')
 desc = fread('data/course_descriptions.csv')
-
-course = import_courses()
-
 
 # Grade Type --------------------------------------------------------------
 
 dbAppendTable(con, 'grade_type', grade_type)
+
+
+# Restriction Type --------------------------------------------------------
+
+dbAppendTable(con, 'restriction_type', restr_type)
+
 
 
 # GEs ---------------------------------------------------------------------
@@ -33,7 +37,7 @@ dbAppendTable(con, 'grade_type', grade_type)
 
 ge_replace = data.table(string=c('\\),', ' or '), replacement=c('\\);', '; '))
 
-GEs = desc$gen_ed[desc$gen_ed!=''] |>
+GEs = desc$gen_ed[!is.na(desc$gen_ed)] |>
   str_multi_replace(ge_replace) |>
   strsplit_vector(';') |>
   str_replace_all('\\.', '') |>
@@ -55,19 +59,13 @@ dbAppendTable(con, 'gen_ed', GEs)
 
 # Learning Activities -----------------------------------------------------
 
-act_misspell = data.table(string=c('Discusson', 'Intership'), 
-                          replacement=c('Discussion', 'Internship'))
-
-desc[, activities:= str_multi_replace(activities, act_misspell)]
-
-
 act_remove = c('hour\\(s\\)', 'hour\\(s\\(', 'hour \\(s\\)', 'hours\\(s\\)',
                '[0-9,\\.\\-]')
 
 act_replace = data.table(string=c(act_remove, '\\s\\s+'), 
                          replacement = c(rep('', length(act_remove)), ' '))
 
-acts = desc$activities[desc$activities!=''] |>
+acts = desc$activities[!is.na(desc$activities)] |>
   tolower() |>
   strsplit_vector('[,;\\] ') |>
   strsplit_vector('/') |>
@@ -86,6 +84,7 @@ learning_activity = data.table(activity = acts)
 
 dbAppendTable(con, 'learning_activity', learning_activity)
 
+#dbSendQuery(con, 'DELETE FROM learning_activity WHERE activity_id > 26;')
 
 
 # Disconnect --------------------------------------------------------------
